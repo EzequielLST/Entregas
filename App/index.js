5fcebd14b8cdfdccfa460cd0211752c304792689
EzequielLST <165871431+EzequielLST@users.noreply.github.com>
2024-07-04 10:50:16 -0300

    entrega final

--- App/index.js
+++ App/index.js
@@ -1,43 +1,10 @@
-const exchangeRates = {
-    ARG: { 
-        EUR: 0.0010,
-        USD: 0.0011,
-        YUAN: 0.0082,
-        REAL: 0.0058,
-    },
-    USD: {
-        EUR: 0.95,
-        ARG: 882.70,
-        YUAN: 7.81,
-        REAL: 5.56,  
-    },
-    EUR: { 
-        USD: 1.18,
-        REAL: 5.56,
-        ARG: 882.70,
-        YUAN: 7.69,
-    },
-    YUAN: { 
-        USD: 0.14, 
-        EUR: 0.13,
-        REAL: 0.71,
-        ARG: 122.14,
-    },
-    REAL: {
-        USD: 0.19,
-        ARG: 171.63,
-        EUR: 0.18,
-        YUAN: 1.41,
-    },
-}; 
-
-document.getElementById('currency-form').addEventListener('submit', function(event) {
+document.getElementById('convertForm').addEventListener('submit', function(event) {
     event.preventDefault();
     convertCurrency();
     displayHistory();
 });
 
-function convertCurrency() {
+async function convertCurrency() {
     const amount = parseFloat(document.getElementById('amount').value);
     
     if (isNaN(amount)) {
@@ -48,21 +15,23 @@ function convertCurrency() {
     const fromCurrency = document.getElementById('fromCurrency').value;
     const toCurrency = document.getElementById('toCurrency').value;
 
-    if (!exchangeRates[fromCurrency] || !exchangeRates[fromCurrency][toCurrency]) {
-        alert("Las tasas de cambio no están definidas para las monedas ingresadas.");
-        return;
-    }
-    
-    const convertedAmount = calculateConversion(amount, fromCurrency, toCurrency);
-    
-    document.getElementById('result').textContent = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
-    
-    saveToHistory(`${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`);
-}
+    try {
+        const response = await fetch(`https://api.getgeoapi.com/v2/currency/convert?api_key=6a3f5530954d7a2b885c3f74cbe7c3428fc19198&from=${fromCurrency}&to=${toCurrency}&amount=${amount}&format=json`);
+        const data = await response.json();
 
-function calculateConversion(amount, fromCurrency, toCurrency) {
-    const rate = exchangeRates[fromCurrency][toCurrency];
-    return amount * rate;
+        if (data.error) {
+            throw new Error(data.error.message);
+        }
+
+        const convertedAmount = parseFloat(data.rates[toCurrency].rate_for_amount);
+
+        document.getElementById('result').textContent = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
+        
+        saveToHistory(`${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`);
+    } catch (error) {
+        console.error("Error en la conversión de moneda:", error);
+        alert("Error en la conversión de moneda");
+    }
 }
 
 function saveToHistory(conversion) {
@@ -89,5 +58,5 @@ function displayHistory() {
     });
 }
 
-
 document.addEventListener('DOMContentLoaded', displayHistory);
+
